5c50d0bde34126e18abbf0b747eb60e2e6637962
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-13 01:06:11 +0300

    Added function to render each book table row

--- src/component_list/Listing.js
+++ src/component_list/Listing.js
@@ -4,13 +4,16 @@ class ListAllBooks extends Component {
 
   render() {
     return (
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
+      <tbody>
+        <tr>
+          <td>{this.props.book.id}</td>
+          <td>{this.props.book.title}</td>
+          <td>{this.props.book.author}</td>
+          <td>{this.props.book.year}</td>
+          <td>{this.props.book.isbn}</td>
+          <td>{this.props.book.price}</td>
+        </tr>
+      </tbody>
     );
   }
 }
