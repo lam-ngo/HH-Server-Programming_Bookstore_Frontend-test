370224427f48339aece914baffab00ad72c5fc42
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-13 01:05:31 +0300

    Added function to render book table

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
 import React, { Component } from 'react';
 import ListAllBooks from './component_list/Listing';
+import booklist from './data';
 import './App.css';
 
 class App extends Component {
 
+
+
   state = {
-    booklist: []
+    booklist: booklist
   };
 
+  /*
   //life-cycle method to fire the function: get data from backend API
   componentDidMount() {
     this.getBooksFromServer();
+    console.log(this.state);
   }
 
   // get data from backend API
+
   getBooksFromServer() {
-      fetch('http://localhost:8080/api/students')
+      fetch('https://lamngo-bookstore.herokuapp.com/books')
       .then((response) => response.json())
       .then((responseData) => {
           this.setState({
-              students: responseData._embedded.students,
+              booklist: responseData._embedded,
           });
       });
   }
+  */
 
   render() {
+
+    var bookListingHandle = this.state.booklist.map(book =>
+      <ListAllBooks key={book.id} book={book} />
+    );
     return (
       <div className="App">
         <table>
-          <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Year</th>
+              <th>ISBN</th>
+              <th>Price</th>
+            </tr>
+          </tbody>
+
+          {bookListingHandle}
         </table>
       </div>
     );
